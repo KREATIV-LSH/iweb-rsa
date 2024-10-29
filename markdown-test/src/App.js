8f7a14dc4f725f733e3578ebea7dddce16e5cbba
Luis Hutterli <lshutterli@gmail.com>
2024-10-29 14:32:37 +0100

    first commit

---
+++ markdown-test/src/App.js
@@ -0,0 +1,49 @@
+import React, { useEffect } from "react";
+import { useRemark } from "react-remark";
+import rehypeRaw from "rehype-raw";
+
+function App() {
+    const [reactContent, setMarkdownSource] = useRemark({
+        rehypePlugins: [rehypeRaw],
+        remarkToRehypeOptions: { allowDangerousHtml: true },
+        rehypeReactOptions: {
+            createElement: React.createElement,
+            components: {
+                p: ({ children }) => {
+                    const hasCustomComponent = React.Children.toArray(children).some((child) => child?.type === TestComponent);
+                    return hasCustomComponent ? children : <p>{children}</p>;
+                },
+                custom: TestComponent,
+                test: Test,
+            },
+        },
+    });
+
+    useEffect(() => {
+        fetch("/mark1.mdx")
+            .then((response) => response.text())
+            .then((text) => setMarkdownSource(text))
+            .catch((error) => console.error("Error fetching markdown:", error));
+    }, [setMarkdownSource]);
+
+    return <>{reactContent}</>;
+}
+
+export default App;
+
+function TestComponent() {
+    return <div style={{ backgroundColor: "#ff00aa", color: "black" }}>This is the Custom component</div>;
+}
+
+function Test({ children }) {
+    return (
+        <div
+            style={{
+                padding: "10px",
+                backgroundColor: "#f0f0f0",
+                borderRadius: "5px",
+            }}>
+            {children}
+        </div>
+    );
+}
---
+++ obsidian/Kapitel.md
@@ -0,0 +1,30 @@
+1. **Verschlüsselungen Einführung**
+	1. Allgemeines zur Verschlüsselung
+		- Quiz: Multiple-Choice Quiz über Verschlüsselungen
+	2. Symmetrische Verschlüsselungen
+	3. Asymmetrische Verschlüsselungen
+		- *Quiz: Multiple-Choice Quiz ob Verschlüsselungen Symmetrisch oder Asymmetrisch* 
+1. **RSA-Verschlüsselung**
+	1. Grundlage / Definition / Mathematik
+		- Interaktives Beispiel: Berechnung von Schlüssel mit gegebenen Primzahlen
+	2. Wieso funktioniert das?
+		- Verweis auf 4.1 Interaktive Visualisierung
+	3. *Kryptowährungen* 
+2. **Implementation**
+	1. Schlüssel-Erstellung
+		1. Primzahl-Generation mit Miller Rabin
+			- Interaktives Beispiel: Nutzer kann Zahlen auf Primalität testen
+		1. *Exkurs Zufällige Zahlen & Wordlist-Ansatz*
+			- *Wordlist: Verweis auf 2.3*
+	1. Ver- & Entschlüsselung
+		- Tool zum Ver- & Entschlüsseln (Auswahl von Schlüssel-Paaren)
+	1. Signierung
+3. *Sicherheit*
+	1. *Schlüsselgrösse*
+		- Interaktive Visualisierung: Wie lange geht es um eine Schlüsselgrösse zu knacken
+	1. *Angriffe & Schwachstellen*
+4. *Optimierung*
+	1. *Mathematisch (CRT)*
+	2. *Hybride Verschlüsselung*
+5. **Andere Verfahren**
+	1.   Elliptic curve cryptography (ECC)
